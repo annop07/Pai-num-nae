07711c947e10b12e627373132e38fe31ee68ab89
sujeephon <sujeephon.p@kkumail.com>
2026-02-17 16:06:19 +0700

    Add Form Incident

---
+++ frontend/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+        <ToastWrapper />
+    </div>
+</template>
+
+<script setup>
+import ToastWrapper from '~/components/ToastWrapper.vue';
+</script>
+
