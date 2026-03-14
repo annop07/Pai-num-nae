ad21ec91f7ff28ff18c42bc2a4726e7dd42a60a6
Suphakit Kaemthong <suphakit.kae@kkumail.com>
2026-03-14 17:53:15 +0700

    addRoute-payment

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,6 +8,7 @@ const bookingRoutes = require('./booking.routes');
 const notificationRoutes = require('./notification.routes');
 const mapRoutes = require('./maps.routes');
 const incidentRoutes = require('./incident.routes');
+const paymentRoutes = require('./payment.routes');
 
 const router = express.Router();
 
@@ -20,5 +21,6 @@ router.use('/bookings', bookingRoutes);
 router.use('/notifications', notificationRoutes);
 router.use('/maps', mapRoutes);
 router.use('/incidents', incidentRoutes);
+router.use('/payments', paymentRoutes);
 
 module.exports = router;
